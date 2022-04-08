3b022edcc60d21e0c75ccf7f7f5435fb318d3fdc
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-08 10:28:34 -0300

    add action buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,17 @@
 
 <h1 class="text-center">CRUD Laravel</h1>
 
+<div class="text-center mt-3 mb-4"><a href=""><button class="btn btn-success">Cadastrar</button></a></div>
+
 <div class="col-8 m-auto">
-    <table class="table">
-        <thead>
+    <table class="table text-center">
+        <thead class="thead-dark">
           <tr>
             <th scope="col">Id</th>
             <th scope="col">Título</th>
             <th scope="col">Autor</th>
             <th scope="col">Preço</th>
+            <th scope="col">Ações</th>
           </tr>
         </thead>
         <tbody>
@@ -25,6 +28,11 @@
                 <td>{{ $books->title }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $books->price }}</td>
+                <td>
+                    <a href=""><button class="btn btn-dark">Vizualizar</button></a>
+                    <a href=""><button class="btn btn-primary">Editar</button></a>
+                    <a href=""><button class="btn btn-danger">Excluir</button></a>
+                </td>
               </tr>
             @endforeach
 
