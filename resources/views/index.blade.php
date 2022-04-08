2f969666a68bcfa6e52ca5149466106644f0bfaa
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-08 09:36:44 -0300

    create a list of all books on index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,4 +4,33 @@
 
 <h1 class="text-center">CRUD Laravel</h1>
 
+<div class="col-8 m-auto">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Título</th>
+            <th scope="col">Autor</th>
+            <th scope="col">Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            @foreach ( $book as $books)
+            @php
+                $user=$books->find($books->id)->relUsers
+            @endphp
+            <tr>
+                <th scope="row">{{ $books->id}}</th>
+                <td>{{ $books->title }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $books->price }}</td>
+              </tr>
+            @endforeach
+
+
+        </tbody>
+      </table>
+</div>
+
 @endsection
