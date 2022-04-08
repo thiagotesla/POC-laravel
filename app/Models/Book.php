25922e659f8dbff90ebc49e60d2f678949cfea30
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-08 09:34:33 -0300

    create Books hasOne User relationship

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $table='books';
+
+    public function relUsers()
+    {
+        return $this->hasOne('App\User', 'id', 'user_id');
+    }
 }
+
+
