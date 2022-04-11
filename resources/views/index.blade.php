520d9bb115c9a95e3ac8c5ea9d1fbf42bbf17b37
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-11 20:43:03 -0300

    change thead style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,16 @@
 
 <h1 class="text-center">CRUD Laravel</h1>
 
-<div class="text-center"><a href="{{url("/books/create")}}"><button class="btn btn-success">Casdastrar Novo</button></a></div>
+<div class="text-center mb-3"><a href="{{url("/books/create")}}"><button class="btn btn-success">Casdastrar Novo</button></a></div>
 
 <div class="col-8 m-auto">
     <table class="table text-center">
-        <thead class="thead-dark">
+        <thead class="table-dark">
           <tr>
             <th scope="col">Id</th>
             <th scope="col">Título</th>
             <th scope="col">Autor</th>
-            <th scope="col">Preço</th>
+            <th scope="col"> Preço</th>
             <th scope="col">Ações</th>
           </tr>
         </thead>
@@ -31,7 +31,7 @@
                 <td>
                     <a href="{{url("books/$books->id")}}"><button class="btn btn-dark">Visualizar</button></a>
                     <a href="{{url("books/$books->id/edit")}}"><button class="btn btn-primary">Editar</button></a>
-                    <a href=""><button class="btn btn-danger">Excluir</button></a>
+                    <a href="{{url("books/$books->id/destroy")}}"><button class="btn btn-danger">Excluir</button></a>
                 </td>
               </tr>
             @endforeach
