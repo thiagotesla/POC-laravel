351184fac9a0e98e549b0be15c62d7820ef86fe8
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-11 19:50:52 -0300

    format document

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,30 +1,53 @@
 @extends('templates.template')
 
 @section('content')
-    <h1 class="text-center">Cadastrar</h1>
-    @if (@isset($errors) && count($errors)>0)
-        <div class="text-center mt-4 mb-4 p-2 alert-danger" >
-            @foreach ( $errors->all() as $error )
+    <h1 class="text-center">
+        @if (@isset($book))
+            Editar
+        @else
+            Cadastrar
+        @endif
+    </h1>
+    @if (@isset($errors) && count($errors) > 0)
+        <div class="text-center mt-4 mb-4 p-2 alert-danger">
+            @foreach ($errors->all() as $error)
                 {{ $error }} <br>
             @endforeach
         </div>
     @endif
 
+    @if (@isset($book))
+        <form name="formEdicao" id="formEdicao" method="post" action="{{ url("/books/$book->id") }}">
+            @method('PUT')
+        @else()
+            <form name="formCadastro" id="FormCadastro" method="post" action="{{ url('/books') }}">
+    @endif
+
     <div class="text-center form-control">
-        <form name="formCadastro" id="FormCadastro" method="post" action="{{url('/books')}}">
-            @csrf
-            <select class="form-control" name="user_id" id="user_id" required>
-                <option class="form-control" value="" selected disabled>Autor</option><br>
-                @foreach ( $users as $user )
-                    <option class="form-control" value="{{$user->id}}">{{$user->name}}</option>
-                @endforeach
-            </select> <br>
-            <input class="form-control" type="text" name="title" id="title" placeholder="Título" required><br>
-            <input class="form-control" type="text" name="pages" id="pages" Placeholder="Número de páginas" required><br>
-            <input class="form-control" type="text" name="price" id="price" placeholder="Preço" required><br>
-            <input class="btn btn-primary mb-3" type="submit" value="Cadastrar">
+
+        @csrf
+        <select class="form-control" name="user_id" id="user_id" required>
+            <option class="form-control" value="{{ $book->relUsers->id ?? '' }}"
+                @if (@isset($book))
+            @else{
+                selected disabled
+            } @endif>
+
+                {{ $book->relUsers->name ?? 'Autor' }}</option><br>
+            @foreach ($users as $user)
+                <option class="form-control" value="{{ $user->id }}">{{ $user->name }}</option>
+            @endforeach
+        </select> <br>
+        <input class="form-control" type="text" name="title" id="title" placeholder="Título"
+            value="{{ $book->title ?? '' }}" required><br>
+        <input class="form-control" type="text" name="pages" id="pages" Placeholder="Número de páginas"
+            value="{{ $book->pages ?? '' }}" required><br>
+        <input class="form-control" type="text" name="price" id="price" placeholder="Preço"
+            value="{{ $book->price ?? '' }}" required><br>
+        <input class="btn btn-primary mb-3" type="submit"
+            value="@if (@isset($book)) Editar @else Cadastrar @endif">
         </form>
     </div>
-    <div class="text-center mt-3"> <a href="{{url("/books")}}"><button class="btn btn-dark">Voltar</button></a></div>
+    <div class="text-center mt-3"> <a href="{{ url('/books') }}"><button class="btn btn-dark">Voltar</button></a></div>
 
 @endsection
