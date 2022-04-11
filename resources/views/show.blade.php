6167498c329f48216fca623b6e69903a540bf396
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-11 19:51:38 -0300

    alter view to improve design

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,17 +4,6 @@
 
 <h1 class="text-center">Visualizar</h1>
 
-
-<div class="col-8 m-auto ">
-
-    Título: {{}}<br>
-    Autor: {{$user->name}} <br>
-    Email: {{$user->email}} <br>
-    Páginas: {{$book->pages}} <br>
-    Preço: {{ $book->price}} <br>
-
-</div>
-
 <div class="col-8 m-auto">
     <table class="table text-center">
         <thead class="thead-dark">
@@ -23,21 +12,26 @@
             <th scope="col">Título</th>
             <th scope="col">Autor</th>
             <th scope="col">Preço</th>
+            <th scope="col">N° páginas</th>
+            <th scope="col">Email autor</th>
           </tr>
         </thead>
          <tbody>
 
 
-            <tr>
-                <th scope="row">{{}}</th>
+           <tr>
+                <th scope="row">{{ $book->id }}</th>
                 <td>{{ $book->title }}</td>
-                <td>{{  }}</td>
-                <td>{{ }}</td>
-              </tr>
+                <td>{{ $user->name }}</td>
+                <td>{{ $book->price }}</td>
+                <td>{{ $book->pages }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
 
 
         </tbody>
       </table>
 </div>
+<div class="text-center mt-3"> <a href="{{url("/books")}}"><button class="btn btn-dark">Voltar</button></a></div>
 
 @endsection
