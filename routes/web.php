9b3c358631e9603ab5fd0bae0cb65115f99596e0
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-11 14:04:03 -0300

    add resource and redirect routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'BookController@index');
+Route::get('/', function () {
+    return redirect('/books');
+});
+
+Route::resource('books', 'BookController');
