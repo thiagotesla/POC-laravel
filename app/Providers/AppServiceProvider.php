2c72fcbdebb0932153a89a07eb4842cc86152974
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-05-24 09:19:11 -0300

    feature/adiciona poc de observer para campo 'ativo'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Book;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Observers\BookObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Book::observe(BookObserver::class);
     }
 }
