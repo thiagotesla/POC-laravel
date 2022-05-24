0244ff327705e9e4ae0a386bbccb540c14d702fc
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-05-24 09:37:39 -0300

    fix/retorna redirect para /books

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect('/books');
+});
 
-
-Route::resource('/', 'BookController');
+Route::resource('books', 'BookController');
